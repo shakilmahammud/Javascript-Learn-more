12c9d13057c2462327de44e6e6c61a52191d1f2c
shakilmahammud <hshakil987@gmail.com>
2020-07-09 18:50:16 +0600

    script commit

---
+++ Learn  Javascipt /custom.js
@@ -0,0 +1,20 @@
+document.write("hello world, extranal js");
+
+//alert ("hello world");
+console.log("hello world"); //output show console
+
+
+document.write("hello world, extranal js");
+
+//this is console log statement
+console.log("hello world"); //output show console
+
+/* ========multiline comment 
+console.log("hello world"); //output show console
+
+
+document.write("hello world, extranal js");
+
+//this is console log statement
+console.log("hello world"); //output show console
+*/ //comment not show the browser
